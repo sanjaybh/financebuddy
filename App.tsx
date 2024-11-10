e449954eb6d160606e99479acacbde56c2a1c60a
Sanjay Bhan <Sanjay.Bhan@discounttire.com>
2024-11-10 18:35:22 -0500

    changes for finance app

---
+++ App.tsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+import { SQLiteProvider } from "expo-sqlite/next";
+import { ActivityIndicator, Platform, Text, View } from "react-native";
+import * as FileSystem from "expo-file-system";
+import { Asset } from "expo-asset";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import Payment from "./screens/sheets/Payment";
+
+const Stack = createNativeStackNavigator();
+
+const loadDatabase = async () => {
+  const dbName = "mySQLiteDB.db";
+  const dbAsset = require("./assets/mySQLiteDB.db");
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+  if (!fileInfo.exists) {
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true }
+    );
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+};
+
+export default function App() {
+  const [dbLoaded, setDbLoaded] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    loadDatabase()
+      .then(() => setDbLoaded(true))
+      .catch((e) => console.error(e));
+  }, []);
+
+  if (!dbLoaded)
+    return (
+      <View style={{ flex: 1 }}>
+        <ActivityIndicator size={"large"} />
+        <Text>Loading Database...</Text>
+      </View>
+    );
+  return (
+    <NavigationContainer>
+      <React.Suspense
+        fallback={
+          <View style={{ flex: 1 }}>
+            <ActivityIndicator size={"large"} />
+            <Text>Loading Database...</Text>
+          </View>
+        }
+      >
+        <SQLiteProvider databaseName="mySQLiteDB.db" useSuspense>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                headerTitle: "Budget Buddy",
+                headerLargeTitle: true,
+                headerTransparent: Platform.OS === "ios" ? true : false,
+                headerBlurEffect: "light",
+              }}
+            />
+            <Stack.Screen
+              name="Payment"
+              component={Payment}
+              options={{
+                presentation: "transparentModal",
+                animation: "slide_from_bottom",
+                animationTypeForReplace: "pop",
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </SQLiteProvider>
+      </React.Suspense>
+    </NavigationContainer>
+  );
+}
---
+++ screens/Home.tsx
@@ -0,0 +1,247 @@
+import * as React from "react";
+import {
+  Button,
+  Platform,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextStyle,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { Category, Transaction, TransactionsByMonth } from "../types";
+import { useSQLiteContext } from "expo-sqlite/next";
+import TransactionList from "../components/TransactionsList";
+import Card from "../components/ui/Card";
+import AddTransaction from "../components/AddTransaction";
+import { BlurView } from "expo-blur";
+import { SymbolView } from "expo-symbols";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import SummaryChart from "../components/SummaryChart";
+
+type StackParamList = {
+  Payment: undefined;
+};
+
+export default function Home() {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<StackParamList, "Payment">>();
+  const [categories, setCategories] = React.useState<Category[]>([]);
+  const [transactions, setTransactions] = React.useState<Transaction[]>([]);
+  const [transactionsByMonth, setTransactionsByMonth] =
+    React.useState<TransactionsByMonth>({
+      totalExpenses: 0,
+      totalIncome: 0,
+    });
+
+  const db = useSQLiteContext();
+
+  React.useEffect(() => {
+    db.withTransactionAsync(async () => {
+      await getData();
+    });
+  }, [db]);
+
+  async function getData() {
+    const result = await db.getAllAsync<Transaction>(
+      `SELECT * FROM Transactions 
+       ORDER BY date DESC
+       LIMIT 30;`
+    );
+    setTransactions(result);
+
+    const categoriesResult = await db.getAllAsync<Category>(
+      `SELECT * FROM Categories;`
+    );
+    setCategories(categoriesResult);
+
+    const now = new Date();
+    // Set to the first day of the current month
+    const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+    // Get the first day of the next month, then subtract one millisecond to get the end of the current month
+    const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+    endOfMonth.setMilliseconds(endOfMonth.getMilliseconds() - 1);
+
+    // Convert to Unix timestamps (seconds)
+    const startOfMonthTimestamp = Math.floor(startOfMonth.getTime() / 1000);
+    const endOfMonthTimestamp = Math.floor(endOfMonth.getTime() / 1000);
+
+    const transactionsByMonth = await db.getAllAsync<TransactionsByMonth>(
+      `
+      SELECT
+        COALESCE(SUM(CASE WHEN type = 'Expense' THEN amount ELSE 0 END), 0) AS totalExpenses,
+        COALESCE(SUM(CASE WHEN type = 'Income' THEN amount ELSE 0 END), 0) AS totalIncome
+      FROM Transactions
+      WHERE date >= ? AND date <= ?;
+    `,
+      [startOfMonthTimestamp, endOfMonthTimestamp]
+    );
+    setTransactionsByMonth(transactionsByMonth[0]);
+  }
+
+  async function deleteTransaction(id: number) {
+    db.withTransactionAsync(async () => {
+      await db.runAsync(`DELETE FROM Transactions WHERE id = ?;`, [id]);
+      await getData();
+    });
+  }
+
+  async function insertTransaction(transaction: Transaction) {
+    db.withTransactionAsync(async () => {
+      await db.runAsync(
+        `
+        INSERT INTO Transactions (category_id, amount, date, description, type) VALUES (?, ?, ?, ?, ?);
+      `,
+        [
+          transaction.category_id,
+          transaction.amount,
+          transaction.date,
+          transaction.description,
+          transaction.type,
+        ]
+      );
+      await getData();
+    });
+  }
+
+  return (
+    <>
+      <ScrollView
+        contentContainerStyle={{
+          padding: 15,
+          paddingVertical: Platform.OS === "ios" ? 170 : 16,
+        }}
+      >
+        {/* <AddTransaction insertTransaction={insertTransaction} /> */}
+        <TransactionSummary
+          totalExpenses={transactionsByMonth.totalExpenses}
+          totalIncome={transactionsByMonth.totalIncome}
+        />
+        <TransactionList
+          categories={categories}
+          transactions={transactions}
+          deleteTransaction={deleteTransaction}
+        />
+      </ScrollView>
+      <BlurView
+        experimentalBlurMethod="dimezisBlurView"
+        intensity={90}
+        tint={"light"}
+        style={styles.blur}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <View>
+            <Text style={{ color: "gray" }}>Lifetime savings</Text>
+            <Text style={{ fontWeight: "bold", fontSize: 28 }}>
+              $123,823.50
+            </Text>
+          </View>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() => navigation.navigate("Payment")}
+          >
+            <SymbolView
+              size={48}
+              type="palette"
+              name="checkmark.circle"
+              colors={["black", "transparent"]}
+              style={{ backgroundColor: "#00000010", borderRadius: 50 }}
+              fallback={
+                <Button
+                  title="open"
+                  onPress={() => navigation.navigate("Payment")}
+                />
+              }
+            />
+          </TouchableOpacity>
+        </View>
+      </BlurView>
+    </>
+  );
+}
+
+function TransactionSummary({
+  totalIncome,
+  totalExpenses,
+}: TransactionsByMonth) {
+  const savings = totalIncome - totalExpenses;
+  const readablePeriod = new Date().toLocaleDateString("default", {
+    month: "long",
+    year: "numeric",
+  });
+
+  // Function to determine the style based on the value (positive or negative)
+  const getMoneyTextStyle = (value: number): TextStyle => ({
+    fontWeight: "bold",
+    color: value < 0 ? "#ff4500" : "#2e8b57", // Red for negative, custom green for positive
+  });
+
+  // Helper function to format monetary values
+  const formatMoney = (value: number) => {
+    const absValue = Math.abs(value).toFixed(2);
+    return `${value < 0 ? "-" : ""}$${absValue}`;
+  };
+
+  return (
+    <>
+      <Card style={styles.container}>
+        {/* <Text style={styles.periodTitle}>Summary for {readablePeriod}</Text> */}
+        <SummaryChart />
+        {/* <Text style={styles.summaryText}>
+          Income:{" "}
+          <Text style={getMoneyTextStyle(totalIncome)}>
+            {formatMoney(totalIncome)}
+          </Text>
+        </Text>
+        <Text style={styles.summaryText}>
+          Total Expenses:{" "}
+          <Text style={getMoneyTextStyle(totalExpenses)}>
+            {formatMoney(totalExpenses)}
+          </Text>
+        </Text>
+        <Text style={styles.summaryText}>
+          Savings:{" "}
+          <Text style={getMoneyTextStyle(savings)}>{formatMoney(savings)}</Text>
+        </Text> */}
+      </Card>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 16,
+    paddingBottom: 7,
+  },
+  blur: {
+    width: "100%",
+    height: 110,
+    position: "absolute",
+    bottom: 0,
+    borderTopWidth: 1,
+    borderTopColor: "#00000010",
+    padding: 16,
+  },
+  periodTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 15,
+  },
+  summaryText: {
+    fontSize: 18,
+    color: "#333",
+    marginBottom: 10,
+  },
+  // Removed moneyText style since we're now generating it dynamically
+});
